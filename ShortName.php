3acda5d43fb056e6270566956e63b2088bde03fd
danielkwood <danielkwood@users.noreply.github.com>
2021-09-28 10:26:11 +1000

    Added comment for the example use

--- ShortName.php
+++ ShortName.php
@@ -6,6 +6,7 @@
         return preg_replace("/[^A-Za-z0-9-]/", "", strtolower(preg_replace("/[^\w]+/", "-", trim($original_string))));
     }
     
+    // Here is an example of how to use this function...
     $test_string = 'Your Original String Goes Here';
     echo ShortName($test_string);
 ?>
